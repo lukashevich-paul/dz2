094d098e14d026d4821218f0d2fba5c26b2c5c2b
lukashevich-paul <161524858+lukashevich-paul@users.noreply.github.com>
2025-08-19 12:34:01 +0300

    fix scaler script
    fix scaler script

--- Assets/Scripts/Scaler.cs
+++ Assets/Scripts/Scaler.cs
@@ -6,33 +6,23 @@ public class Scaler : MonoBehaviour
     [SerializeField] private float _maxScale = 2f;
     [SerializeField] private float _scaleSpeed = 2.5f;
 
-    private float _targetScale;
+    private float _currentSpeed;
 
     private void Start()
     {
-        _targetScale = _maxScale;
+        _currentSpeed = _maxScale;
     }
 
     private void Update()
     {
         Vector3 scale = transform.localScale;
 
-        if (scale.x > _maxScale)
+        if (scale.x > _maxScale || scale.x < _minScale)
         {
-            _targetScale = -_scaleSpeed;
-        }
-        else if (scale.x < _minScale)
-        {
-            _targetScale = _scaleSpeed;
+            _currentSpeed *= -1;
         }
 
-        float newValue = _targetScale * Time.deltaTime;
-        Scale(newValue);
-    }
-
-    private void Scale(float value)
-    {
-        float newValue = _targetScale * Time.deltaTime;
-        transform.localScale += new Vector3(value, value, value);
+        float newValue = _currentSpeed * Time.deltaTime;
+        transform.localScale += Vector3.one * newValue;
     }
 }
